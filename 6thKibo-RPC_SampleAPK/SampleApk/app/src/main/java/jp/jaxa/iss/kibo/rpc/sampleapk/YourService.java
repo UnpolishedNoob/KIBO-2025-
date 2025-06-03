4b81870c763b86513e7ef3e44bdc8a418c9bc1ce
UnpolishedNoob <banikpritom321@gmail.com>
2025-06-03 21:26:50 +0600

    final traveling point

--- 6thKibo-RPC_SampleAPK/SampleApk/app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/YourService.java
+++ 6thKibo-RPC_SampleAPK/SampleApk/app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/YourService.java
@@ -11,9 +11,18 @@ import gov.nasa.arc.astrobee.types.Point;
 import gov.nasa.arc.astrobee.types.Quaternion;
 
 
+import org.opencv.aruco.Aruco;
+import org.opencv.aruco.Dictionary;
+import org.opencv.calib3d.Calib3d;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
 
 //import java.util.Dictionary;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -24,17 +33,27 @@ public class YourService extends KiboRpcService {
     private final String TAG = this.getClass().getSimpleName();
 
     private final double[][] place={
-            {10.7,-9.79,4.95},
-            {10.91,-8.875,4.55},
-            {10.535,-7.88,4.55},
-            {10.52,-6.7,4.95}
+            {10.95,-9.85,5.195},
+            {10.925,-8.875,4.52},
+            {10.942,-7.89,4.51},
+            {10.6,-6.852,4.94}
     };
 
     private final float[][] angle={
-            {0,0,-707f,707f},
-            {0,707f,0,707f},
-            {0,707f,0,707f},
+            {0,0,-0.707f,0.707f},
+            //{0,0.707f,0,0.707f},
+            {-0.0923f,0.7002f,-0.0923f,0.7002f},
+            //{0,0.707f,0,0.707f},
+            {0.1651f,0.6876f,0.1651f,0.6876f},
             {0,1f,0,0}
+    };
+
+    private String[] image={
+            "image_1.jpg",
+            "image_2.jpg",
+            "image_3.jpg",
+            "image_4.jpg",
+            "final.jpg"
     };
 
     @Override
@@ -52,7 +71,7 @@ public class YourService extends KiboRpcService {
             Point point=new Point(x,y,z);
             Quaternion q=new Quaternion(a,b,c,d);
             boolean ans=move(point,q,i);
-            Log.i(TAG,"ans: "+i+" moving to is : "+ans);
+            Log.i(TAG," moved to: "+point+" -> "+ans);
         }
 
 
@@ -67,7 +86,8 @@ public class YourService extends KiboRpcService {
         // When you move to the front of the astronaut, report the rounding completion.
          Point point1 = new Point(11.143d, -6.7607d, 4.9654d);
         Quaternion quaternion1 = new Quaternion(0f, 0f, 0.707f, 0.707f);
-        api.moveTo(point1, quaternion1, false);
+        boolean ans=move(point1,quaternion1,5);
+        //api.moveTo(point1, quaternion1, false);
         api.reportRoundingCompletion();
 
         /* ********************************************************** */
@@ -115,10 +135,11 @@ public class YourService extends KiboRpcService {
             result = api.moveTo(point, quaternion, true);
 
             if(result.hasSucceeded()){
-                wait(1500);
+                wait(1000);
                 Mat img=api.getMatNavCam();
                 wait(10);
-                api.saveMatImage(img,i+".jpg");
+                api.saveMatImage(img,"img@"+i+".jpg");
+                //ar_read_crop(img,i);
 
                 return true;
             }
@@ -126,7 +147,7 @@ public class YourService extends KiboRpcService {
 
             Log.i(TAG,"Move attempt :" + retryCount);
 
-            wait(1500);
+            wait(1000);
         } while(!result.hasSucceeded() && retryCount < 3);
 
         Log.i(TAG, "Move to target failed");
@@ -144,6 +165,67 @@ public class YourService extends KiboRpcService {
         }
 
         return;
+    }
+
+    //Aruco tag detection
+    public void ar_read_crop(Mat mat,int i){
+        //Get camera matrix
+        Mat cameraMatrix=new Mat(3,3, CvType.CV_64F);
+        cameraMatrix.put(0,0,api.getNavCamIntrinsics()[0]);
+
+        //Get lens distortion parameter
+        Mat cameraCoefficient=new Mat(1,5,CvType.CV_64F);
+        cameraCoefficient.put(0,0,api.getNavCamIntrinsics()[1]);
+        cameraCoefficient.convertTo(cameraCoefficient,CvType.CV_64F);
+
+        //Undistort image
+        Mat undistorting=new Mat();
+        Calib3d.undistort(mat,undistorting,cameraMatrix,cameraCoefficient);
+
+        Dictionary dictionary= Aruco.getPredefinedDictionary(Aruco.DICT_5X5_250);
+        List<Mat>corners=new ArrayList<>();
+        Mat markerids=new Mat();
+        Aruco.detectMarkers(mat,dictionary,corners,markerids);
+        if (!markerids.empty()) {
+            crop(mat, corners, markerids, i);
+        } else {
+            Log.i(TAG, "No ArUco markers detected in image " + image[i]);
+        }
+    }
+
+    public void crop(Mat img, List<Mat> corners, Mat Markerids, int i) {
+        // I want to crop like a box which will have 32 centimeter height and width,
+        // which is parallel to corners of the aruco tag
+        // and the center of aruco tag is center of the box
+        double x = 0,y =0;
+        if (!corners.isEmpty()) {
+            Mat cornerMat = corners.get(0); // Get the first marker's corner matrix (4x1x2)
+
+            double[] tl = cornerMat.get(0, 0); // [x, y] // Get the top-left corner: index 0
+
+            if (tl != null && tl.length >= 2) {
+                 x =  tl[0];
+                 y =  tl[1];
+
+                Log.i(TAG, "Top-left corner: x = " + x + ", y = " + y);
+            } else {
+                Log.i(TAG, "Top-left corner not found or malformed.");
+            }
+        } else {
+            Log.i(TAG, "No ArUco markers detected.");
+            return;
+        }
+        x-=23;
+        y-=23;
+           Log.i(TAG,"top left of croped image : "+x+" "+y);
+        // Define the crop rectangle: x, y, width, height
+        Rect roi = new Rect((int)x, (int)y, 200, 150);
+
+        // Crop the region
+        Mat cropped = new Mat(img, roi);
+        api.saveMatImage(cropped,"cropped_"+i+".png");
+        return;
+
     }
 
 }
